15eb0aba9c60d366aa48336cac3fedd39c588a81
Sharath <sharath>
2019-06-16 21:09:51 +0530

    added tabs

--- android/app/src/main/java/com/haisaa/MainApplication.java
+++ android/app/src/main/java/com/haisaa/MainApplication.java
@@ -3,6 +3,7 @@ package com.haisaa;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.horcrux.svg.SvgPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReanimatedPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
             new SvgPackage(),
