bddd396e1d8d3c67a5d4a8ec17d2454f4a92b087
Sharath <sharath>
2019-08-24 22:33:45 +0530

    GPS with context api

--- components/helpRequest/helpRequestFeed.js
+++ components/helpRequest/helpRequestFeed.js
@@ -2,49 +2,17 @@ import React, { Component } from "react";
 import firebase from "react-native-firebase";
 import { ScrollView } from "react-native-gesture-handler";
 import HelpRequest from "./helpRequest";
-import { getDistanceFromLatLonInKm } from "../../utils";
-import geolocation from 'react-native-geolocation-service';
+import Context from "../../context";
 
 class HelpRequestFeed extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      helpRequests: [],
-      latitude: null,
-      longitude: null,
-      locationProviderAvailable: false,
-      locationErrorMessage:""
+      helpRequests: []
     };
   }
 
   componentDidMount() {
-    // geolocation.getCurrentPosition(
-    //   (position) => {
-    //     console.log(position.coords.latitude, position.coords.longitude);
-    //       this.setState({
-    //         latitude:position.coords.latitude,
-    //         longitude:position.coords.longitude,
-    //         locationProviderAvailable: true
-    //       });
-    //   },
-    //   (error) => {
-    //       // See error code charts below.
-    //       console.log(error.code, error.message);
-    //       let locationErrorMessage;
-    //       switch(error.code){
-    //         case 1:locationErrorMessage="Location permission is not granted";break;
-    //         case 2:locationErrorMessage="Location provider not available";break;
-    //         case 3:locationErrorMessage="Location request timed out";break;
-    //         case 4:locationErrorMessage="Google play service is not installed or has an older version";break;
-    //         case 5:locationErrorMessage="Location service is not enabled or location mode is not appropriate for the current request";break;
-    //       }
-    //       this.setState({
-    //         locationErrorMessage,
-    //         locationProviderAvailable: false
-    //       });
-    //   },
-    //   { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    // );
     const {db} = this.props;
     firebase.database().ref(`${db}`).on('child_moved', (data) => {
       console.log(data.val());
@@ -60,12 +28,6 @@ class HelpRequestFeed extends Component {
       this.setState({ helpRequests: newHelpRequests });
     });
   }
-
-  handleLogOut = () => {
-    const { navigation } = this.props;
-    firebase.auth().signOut();
-    navigation.navigate("Login", {});
-  };
 
   getHelpRequests = () => {
     const { helpRequests } = this.state;
@@ -90,4 +52,5 @@ class HelpRequestFeed extends Component {
   }
 }
 
+HelpRequestFeed.contextType = Context;
 export default HelpRequestFeed;
