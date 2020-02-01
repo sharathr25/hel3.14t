16e829b5335b0fb158ed3eb22f0ce109550e8a9d
sharathr25 <50552771+sharathhel314t@users.noreply.github.com>
2020-02-01 14:30:36 +0530

    account and signup screen

---
+++ components/common/Error.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { RED, FLAG_COLOR_WHITE } from '../../constants/styleConstants';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const Error = (props) => {
+    const { text = "Something went wrong, please try again later..." } = props;
+    const { container, textStyle, icon } = styles;
+    return (
+        <View style={container}>
+            <View style={icon}>
+                <Icon size={25} color={FLAG_COLOR_WHITE} name="exclamation" />
+            </View>
+            <Text style={textStyle}>{text}</Text>
+        </View>
+    );
+}
+
+export default Error;
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1
+    },
+    textStyle: {
+        fontSize: 25,
+        textAlign: 'center'
+    },
+    icon: {
+        backgroundColor: RED,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
