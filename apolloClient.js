5f72c1314289f1c770717816a32c0e60c7ddb4f5
sharathr25 <50552771+sharathhel314t@users.noreply.github.com>
2020-03-14 18:41:58 +0530

    added auth

--- apolloClient.js
+++ apolloClient.js
@@ -5,18 +5,14 @@ import { HttpLink } from 'apollo-link-http';
 import { getMainDefinition } from 'apollo-utilities';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { config } from './config';
-import AsyncStorage from '@react-native-community/async-storage';
 
 const { dev, prod } = config;
 
-// const httpEndPoint = process.env.NODE_ENV === "development" ? dev.SERVER_HTTP_END_POINT : prod.SERVER_HTTP_END_POINT;
-// const wsEndPoint = process.env.NODE_ENV === "development" ? dev.SERVER_WEB_SOCKET_END_POINT : prod.SERVER_WEB_SOCKET_END_POINT;
+const httpEndPoint = process.env.NODE_ENV === "development" ? dev.SERVER_HTTP_END_POINT : prod.SERVER_HTTP_END_POINT;
+const wsEndPoint = process.env.NODE_ENV === "development" ? dev.SERVER_WEB_SOCKET_END_POINT : prod.SERVER_WEB_SOCKET_END_POINT;
 
 // const httpEndPoint = prod.SERVER_HTTP_END_POINT;
 // const wsEndPoint = prod.SERVER_WEB_SOCKET_END_POINT;
-
-const httpEndPoint = dev.SERVER_HTTP_END_POINT;
-const wsEndPoint = dev.SERVER_WEB_SOCKET_END_POINT;
 
 const withAuthToken = (token = "") => {
   // Create an http link:
